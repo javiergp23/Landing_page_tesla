c1c1d1502de5d75a0467eaf5bad9071acbb391f1
javiergp23 <javgon23@hotmail.com>
2023-07-12 00:03:49 -0300

    deploy-changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://javiergp23.github.io/',
+  base: '/Landing_page_tesla/',
   integrations: [tailwind()]
 });
